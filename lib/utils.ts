2b9ea51d28466977559d10fe4a7948eecfffd486
zakariyo <zakaretosmatov@gmail.com>
2026-06-21 14:56:44 +0600

    fix: install clsx and tailwind-merge and update cn helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
 import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return inputs.filter(Boolean).join(' ');
+  return twMerge(clsx(inputs));
 }
 
 export function formatPrice(price: number): string {
